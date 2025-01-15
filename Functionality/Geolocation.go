00ef0d3e8af88347b2d9aab0192b17ab233b732f
Wafula-Ian01 <wafula-ian01@github.com>
2025-01-15 10:34:16 +0300

    geolocation feature

---
+++ Functionality/Geolocation.go
@@ -0,0 +1,39 @@
+package Functionality
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Geolocation struct {
+	Query   string  `json:"query"`      // Public IP
+	Country string  `json:"country"`    // Country
+	Region  string  `json:"regionName"` // Region
+	City    string  `json:"city"`       // City
+	Lat     float64 `json:"lat"`        // Latitude
+	Lon     float64 `json:"lon"`        // Longitude
+	ISP     string  `json:"isp"`        // Internet Service Provider
+}
+
+func getGeolocation(ip string) (*Geolocation, error) {
+	//using ip-api.com to obtain public ip of device
+	url := fmt.Sprintf("http://ip-api.com/json/%s", ip)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var location Geolocation
+	if err := json.Unmarshal(body, &location); err != nil {
+		return nil, err
+	}
+	return &location, nil
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BlitzPubSec
+- BlitzPubSec is Public Security OSINT application built to provide initial assistance to track devices 
+with eSIMs using information such as device public IP's which helps provides device's location. 
+####  It's still a work in progress and contributions are welcome.
