5cdd29302bebdfc45d689234fc2802eef53082ac
Wafula-Ian01 <wafula-ian01@github.com>
2025-01-15 12:40:20 +0300

    getting ip address

--- Functionality/Geolocation.go
+++ Functionality/Geolocation.go
@@ -19,12 +19,17 @@ type Geolocation struct {
 
 func getGeolocation(ip string) (*Geolocation, error) {
 	//using ip-api.com to obtain public ip of device
-	url := fmt.Sprintf("http://ip-api.com/json/%s", ip)
+	url := fmt.Sprintf("https://ip-api.com/json/%s", ip)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
--- Functionality/IP.go
+++ Functionality/IP.go
@@ -1,1 +1,26 @@
 package Functionality
+
+import (
+	"io"
+	"net/http"
+)
+
+func getIP() (string, error) {
+	resp, err := http.Get("https://api.ipify.org?format=text")
+	if err != nil {
+		return "", nil
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	ip, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil
+	}
+
+	return string(ip), nil
+}
