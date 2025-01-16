7ac0cb0b32c569be7efdb44659c8b0645a1f945f
Wafula-Ian01 <wafula-ian01@github.com>
2025-01-16 11:51:59 +0300

    small fix

--- Functionality/Geocoding.go
+++ Functionality/Geocoding.go
@@ -8,40 +8,39 @@ import (
 )
 
 type Location struct {
-	Name    string  `json:"name"`    //Address Display name
-	Address Address `json:"address"` //Location Address details
-	Class   string  `json:"class"`   //Structural type
-	Type    string  `json:"type"`    //Type
+	Name    string  `json:"display_name"` //Address Display name
+	Address Address `json:"address"`      //Location Address details
+	Class   string  `json:"class"`        //Structural type
+	Type    string  `json:"type"`         //Type
 }
 
 type Address struct {
-	Road          string  `json:"road"`          // Street
-	Neighbourhood string  `json:"neighbourhood"` // Neighbourhood
-	Village       string  `json:"village"`       // Region
-	City          string  `json:"city"`          // City
-	State         float64 `json:"state"`         // Region
-	Country       float64 `json:"country"`       // Country
-	Code          string  `json:"country_code"`  // Country Code
+	Road          string `json:"road"`          // Street
+	Neighbourhood string `json:"neighbourhood"` // Neighbourhood
+	Village       string `json:"village"`       // Region
+	City          string `json:"city"`          // City
+	State         string `json:"state"`         // Region
+	Country       string `json:"country"`       // Country
+	Code          string `json:"country_code"`  // Country Code
 }
 
-func (geo Geolocation) GetLocation() (string, error) {
+func GetLocation(lat, lon float64) (*Location, error) {
 	//making call to nominatim osm api
-	lat, lon := geo.Lat, geo.Lon
 	url := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?lat=%f&lon=%f&format=json", lat, lon)
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", nil
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", nil
+		return nil, err
 	}
 	var locality Location
 	if err := json.Unmarshal(body, &locality); err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return "", nil
+	return &locality, nil
 }
