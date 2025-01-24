29a9db4763bfe424318e75a2b59e8875f76f6930
Wafula-Ian01 <wafula-ian01@github.com>
2025-01-24 12:55:59 +0300

    terminal init

---
+++ Functionality/Terminal.go
@@ -0,0 +1,29 @@
+package functionality
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+)
+
+func getOption(r io.Reader, w io.Writer) (string, error) {
+	msg := "Blitz Pub Sec \n Choose one of the options below:\n" +
+		" \t 1. Get my device's details\n \t 2. Get another device's details\n" +
+		"To select an option enter it's number below to proceed. Thanks\n"
+	fmt.Fprint(w, msg)
+
+	scanner := bufio.Scanner
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	option := scanner.Text()
+
+	if len(option) == 0 {
+		return "", errors.New("You cannot proceed without an option selected!")
+	}
+
+	return "", nil
+}
